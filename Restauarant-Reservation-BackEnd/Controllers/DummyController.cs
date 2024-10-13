7f715466280813f00f29cf8583f6abf3b8776395
KariyavulaMeghana <meghana8767@gmail.com>
2024-10-13 18:36:50 +0530

    Added dummycontroller for test purpose

---
+++ Restauarant-Reservation-BackEnd/Controllers/DummyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restauarant_Reservation_BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DummyController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("Working");
+        }
+    }
+}
