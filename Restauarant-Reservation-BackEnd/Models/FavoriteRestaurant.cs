bcc4a920658e47382f4d2dc4aca3b37de1e3e99c
Nandinilukka <nandinilukka@0403gmail.com>
2024-10-15 19:11:53 +0530

    models favorite restaurant,restaurant,review,user

---
+++ Restauarant-Reservation-BackEnd/Models/FavoriteRestaurant.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restauarant_Reservation_BackEnd.Models
+{
+    public class FavoriteRestaurant
+    {
+        [Key]
+        public int FavoriteId { get; set; }
+        public virtual User? User { get; set; }
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public virtual Restaurant? Restaurant { get; set; }
+        [ForeignKey(nameof(Restaurant))]
+        public int RestaurantId { get; set; }
+    }
+}
---
+++ Restauarant-Reservation-BackEnd/Models/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restauarant_Reservation_BackEnd.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantID { get; set; }
+        [Required(ErrorMessage = "Enter your Name")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "name should contains 3 to 15 chars")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "only letters are allowed")]
+        public string? RestaurantName { get; set; }
+        [Required(ErrorMessage = "enter description")]
+        public string? Description { get; set; }
+        [Required(ErrorMessage = "enter your location")]
+        public string? Location { get; set; }
+        [Required(ErrorMessage = "enter your PhoneNumber")]
+        [Phone(ErrorMessage = "Invaid phone number")]
+        public string? ContactNumber { get; set; }
+        public double Rating { get; set; }
+        public virtual ICollection<Review>? Reviews { get; set; }
+    }
+}
